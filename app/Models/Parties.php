14578a69c8f47a277ff6adc8aa15db9126afeebe
Nagarjunl <naga.career.at@gmail.com>
2024-10-10 20:00:24 +0530

    first commit

---
+++ app/Models/Parties.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Parties extends Model
+{
+     protected $primaryKey = 'id';
+     protected $fillable = [
+          'id',
+          'party_name',
+          'mobile_number',
+          'email',
+          'opening_balance',
+          'party_type',
+          'gst_number',
+          'state',
+          'billing_address',
+          'shipping_address',
+          'credit_period',
+          'credit_limit',
+    ];
+}
+
+
---
+++ database/migrations/2024_10_10_080317_create_parties_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('parties', function (Blueprint $table) {
+            $table->id()->autoIncrement();
+            $table->string('party_name');
+            $table->string('mobile_number')->nullable();
+            $table->string('email')->nullable();
+            $table->integer('opening_balance')->default(0);
+            $table->string('party_type');
+            $table->string('gst_number')->nullable();
+            $table->string('state');
+            $table->string('billing_address')->nullable();
+            $table->string('shipping_address')->nullable();
+            $table->string('credit_period')->default(0);
+            $table->integer('credit_limit')->default(0);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('parties');
+    }
+};
